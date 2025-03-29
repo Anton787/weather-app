adb85dfd3c56b29742d7451fcba8b9ac3ee8200a
Anton787 <antongagelganc6443@gmail.com>
2025-03-29 14:24:47 +0500

    first commit

---
+++ app/main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 560,
+    height: 720,
+    webPreference: {
+      nodeIntegretion: true,
+    },
+  });
+
+  mainWindow.loadFile('./app/index.html')
+}
+
+app.whenReady().then(() =>{
+  createWindow();
+  app.on('activate', () => {
+    if(BrowserWindow.getAllWindows().length === 0){
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  };
+});
